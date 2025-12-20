694667f2650f7c6549d7fb220a7c8674075d2ffb
suryatejarampuram <suryarampuram121@gmail.com>
2025-12-20 16:23:00 +0530

    og tags

--- update_nginx_api.sh
+++ update_nginx_api.sh
@@ -31,6 +31,39 @@ server {
         proxy_cache_bypass \$http_upgrade;
     }
 
+    # NEW: Proxy Uploads to Backend Port 4000
+    location /uploads {
+        proxy_pass http://localhost:4000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+
+    # NEW: Server-Side OG Tags for Bots
+    location /blogs/ {
+        error_page 418 = @bot_backend;
+        recursive_error_pages on;
+
+        # Detect Social Media Bots
+        if (\$http_user_agent ~* "facebookexternalhit|linkedinbot|twitterbot|whatsapp|telegrambot|googlebot|bingbot|discordbot") {
+            return 418;
+        }
+
+        # For Humans: Serve React App
+        try_files \$uri \$uri/ /index.html;
+    }
+
+    location @bot_backend {
+        proxy_pass http://localhost:4000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+
     listen 443 ssl; # managed by Certbot
     ssl_certificate /etc/letsencrypt/live/www.resrishti.com/fullchain.pem; # managed by Certbot
     ssl_certificate_key /etc/letsencrypt/live/www.resrishti.com/privkey.pem; # managed by Certbot
